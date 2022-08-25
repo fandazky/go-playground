5fd82d8fa41ad9376b2b9eab861e58cf898e3560
Fandy Ahmad <fandazky@gmail.com>
2022-08-25 14:14:40 +0700

    add external dependency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fandazky/go-playground
+
+go 1.19
+
+require github.com/fandazky/go-module-introduction v1.1.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_module_introduction "github.com/fandazky/go-module-introduction"
+)
+
+func main() {
+	fmt.Println(go_module_introduction.SayHello())
+}
